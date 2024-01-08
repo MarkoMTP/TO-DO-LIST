75da0688374e24b50cbcd84bafcd9d953cdc327c
MarkoMTP <marko.mtp8@gmail.com>
2024-01-08 22:13:27 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    mode: "development", 
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: "./src/template.html", // Specify your HTML template file
+        title: 'To-Do List',
+    }),
+   
+],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+
+  },
+  
+  module: {
+    rules: [ 
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+        
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+  ],
+     },
+};
